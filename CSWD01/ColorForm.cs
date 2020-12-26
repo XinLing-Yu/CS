e03f83f5d13e437b019a901e9b6caf2fe68a107d
XinLing-Yu <103304325@qq.com>
2020-12-26 15:31:53 +0800

    CSWD1226

---
+++ CSWD01/ColorForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSWD01
+{
+    public partial class ColorForm : Form
+    {
+        public ColorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ColorForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.BackColor = Color.Black;
+        }
+
+        private void ColorForm_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.BackColor = Color.Blue;
+        }
+
+        private void ColorForm_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Gray;
+        }
+    }
+}
